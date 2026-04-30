3cdeadcfec1d0ab5325869215126b866c859d4f1
acamberos1 <camberos.athian1@gmail.com>
2026-04-30 11:24:53 -0700

    login almost done

--- app/src/main/java/com/example/decisionwheel/LoginActivity.java
+++ app/src/main/java/com/example/decisionwheel/LoginActivity.java
@@ -1,4 +1,21 @@
 package com.example.decisionwheel;
 
+
+import  android.content.Context;
+import java.util.ArrayList;
+import java.util.List;
+import android.content.Intent;
+import android.widget.Toast;
+import  android.os.Bundle;
+import  android.view.View;
+import androidx.lifecycle.LiveData;
+
+
+
 public class LoginActivity {
+
+
+    // will grab username and password
+    // needs xml too so make sure to work on that
+    //
 }

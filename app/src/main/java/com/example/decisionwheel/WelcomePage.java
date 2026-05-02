b48bcd14e09d4bd20042b0dea8f66e641cb48c72
acamberos1 <camberos.athian1@gmail.com>
2026-05-01 22:36:38 -0700

    bugfix

--- app/src/main/java/com/example/decisionwheel/WelcomePage.java
+++ app/src/main/java/com/example/decisionwheel/WelcomePage.java
@@ -1,24 +1,30 @@
 package com.example.decisionwheel;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class WelcomePage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        findViewById(R.id.loginButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(LoginActivity.loginIntentFactory(getApplicationContext()));
+            }
+        });
+
+        findViewById(R.id.makeAccButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // todo: create account
+            }
         });
     }
 }
